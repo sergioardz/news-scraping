d5d8f519a3931f6502d26d3529bbeafc52778741
Sergio Armando Rodriguez Ramirez <sergioardz79@gmail.com>
2019-07-04 12:26:12 -0500

    Created basic file structure and started server

---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require("express");
+var mongoose = require("mongoose");
+
+var PORT = 3000;
+
+// Initialize Express
+var app = express();
+
+// Configure middleware
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Make public a static folder
+app.use(express.static("public"));
+
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+
+
+// Start the server
+app.listen(PORT, function () {
+	console.log("App running on port " + PORT + "!");
+});
